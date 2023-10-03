582ced9ca5805466f80ab7f6da087e5de5149c72
Alexandr <134552389+Alexsergh@users.noreply.github.com>
2023-10-03 16:22:20 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# 1)
+# a = 1
+# print(a == 2)
+
+# 2)
+# a, b, c = 1, 2, 3
+# print(a, b, c)
+
+# 3)
+# a = int(input())
+# print(a)
+
+# 4)
+# a = 'abcde'
+# print(a*4)
+
+#5)
+# day, month, year = 23, 'октября', 2020
+# print(f'Сегодня {day} {month} {year}.',end='Всего хорошего!')
+
+#6)
+# a, b = 'Hello', 'world'
+# print(a, b, sep=' my ')
+
+#7)
+# sen = 'Hello world'
+# print(len(sen))
+
+#8)
+# sen = 'HELLO WORLD'
+# print(sen.lower())
+
+#9)
+# a, b, c = 135, 10, 1234
+# print(f'сумма переменных={a + b + c}\nразность переменных={a - b - c}\nумножение переменных={a * b * c}\nцелочисленное деление переменных={a // b // c}')
+
+#10)
+# a, b, c, d = 'hello ', 'my ', 'name is ', 'Peter '
+# print(a + b.upper() + c.title() + d.lower())
